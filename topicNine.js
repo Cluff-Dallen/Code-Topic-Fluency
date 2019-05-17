85047d0e26ce46a5fbb324f9e6cd076154a88bde
Cluff-Dallen <cluffrdallen@gmail.com>
2019-05-16 18:08:57 -0600

    More updates to topicNine.html/.js and addition of several images.

--- topicNine.js
+++ topicNine.js
@@ -1,8 +1,7 @@
 function f1() {
-    alert("Function 1 Called!");
-
     var color = document.body.style.backgroundColor;
 
+    //if the background is already white, make it pink. And vise versa!
     if (color == 'white') {
         color = 'pink';
     } else {
@@ -13,7 +12,8 @@ function f1() {
 }
 
 function f2() {
-    alert("Function 2 Called!");
+
+    //make the background the colore chosen by the radio input each time it's changed.
     if (document.getElementById('radio1').checked) {
         document.body.style.backgroundColor = 'green';
     }
@@ -29,19 +29,29 @@ function f2() {
 }
 
 function f3() {
-    alert("Function 3 Called!");
-
+    //when a key is pressed on the webpage, make the bunny disappear
+    alert("You've called the keydown event, now the bunny has disappeared!");
+    document.getElementById("keydown1").innerHTML = "And just like that, your keypress triggered the event that made this bunny disappear!";
 }
 
 function f4() {
-    alert("This alert is shwowing because you've triggered the onload event!");
+    //this is triggered upon page load, but load text and alert.
+    alert("This alert is showing because you've triggered the onload event!");
     document.getElementById("onLoad1").innerHTML = "The only reason this text is showing, is because the onLoad event was triggered! Otherwise this would be blank.";
 }
 
 function f5() {
-    alert("Function 5 Called!");
-
+    //add an image upon hovering over button.
     document.getElementById("imgHover").innerHTML = "<img src='softwarefunny.jpeg'>";
+}
 
+function f6() {
+    //change the image to the flipped version of the picture by replacing the text upon touch screen press. 
+    document.getElementById("flip").innerHTML = '<img src="rightside.jpeg" alt="right side up book of mormon" ontouchstart="f6()" width="200px" height="200px"></img>';
+}
 
+function f7() {
+    //when the user removes finger from touch screen, change the colore to red and change the text.
+    document.getElementById("touchend1").style.background = "red";
+    document.getElementById("touchend1").innerHTML = "You changed this text by making your finger leave!";
 }
