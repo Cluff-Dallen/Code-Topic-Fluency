a3063e4a831e57f60d1f851385d9ece2c39728fa
Cluff-Dallen <cluffrdallen@gmail.com>
2019-06-28 10:34:58 -0600

    Updated and completed topicEight

--- topicEight.js
+++ topicEight.js
@@ -5,3 +5,26 @@ function f1() {
     square.style.width = '200px';
     square.style.height = '200px';
 }
+
+function f2() {
+
+    var bar = document.getElementById("jsAnimation");
+
+    var x = setInterval(move, 1);
+
+    var p = 1;
+
+    function move() {
+
+        if (p != 100) {
+            bar.style.left = p + 'px';
+            p++;
+        }
+
+    }
+}
+
+function f3() {
+    document.getElementById("square").style.transition = "all 2s";
+    document.getElementById("square").style.transform = "skew(25deg)";
+}
